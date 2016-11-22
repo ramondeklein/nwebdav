ramondeklein/nwebdav

commit 9e314680ab9b0b957897945828bd98b7752ad7f3

Improve documentation.

NWebDav.Server/Helpers/RequestHelper.cs
@@ -10,44 +10,80 @@ using NWebDav.Server.Logging;
 
 namespace NWebDav.Server.Helpers
 {
+    /// <summary>
+    /// Splitted URI consisting of a collection URI and a name string.
+    /// </summary>
     public class SplitUri
     {
+        /// <summary>
+        /// Collection URI that holds the collection/document.
+        /// </summary>
         public Uri CollectionUri { get; set; }
+
+        /// <summary>
+        /// Name of the collection/document within its container collection.
+        /// </summary>
         public string Name { get; set; }
     }
 
+    /// <summary>
+    /// Range
+    /// </summary>
     public class Range
     {
+        /// <summary>
+        /// Optional start value.
+        /// </summary>
         public long? Start { get; set; }
+
+        /// <summary>
+        /// Optional end value.
+        /// </summary>
         public long? End { get; set; }
+
+        /// <summary>
+        /// Optional conditional date/time.
+        /// </summary>
         public DateTime If {get; set; }
     }
 
+    /// <summary>
+    /// Helper methods for <see cref="IHttpRequest"/> objects.
+    /// </summary>
     public static class RequestHelper
     {
         private static readonly Regex s_rangeRegex = new Regex("bytes\\=(?<start>[0-9]*)-(?<end>[0-9]*)");
-        private static readonly ILogger s_log = LoggerFactory.CreateLogger(typeof(RequestHelper));
 
-        public static SplitUri SplitUri(string uri)
+        /// <summary>
+        /// Split an URI into a collection and name part.
+        /// </summary>
+        /// <param name="uri">URI that should be splitted.</param>
+        /// <returns>
+        /// Splitted URI in a collection URI and a name string.
+        /// </returns>
+        public static SplitUri SplitUri(Uri uri)
         {
             // Determine the offset of the name
-            var slashOffset = uri.LastIndexOf('/');
+            var slashOffset = uri.AbsoluteUri.LastIndexOf('/');
             if (slashOffset == -1)
                 return null;
 
             // Separate name from path
             return new SplitUri
             {
-                CollectionUri = new Uri(uri.Substring(0, slashOffset)),
-                Name = Uri.UnescapeDataString(uri.Substring(slashOffset + 1))
+                CollectionUri = new Uri(uri.AbsoluteUri.Substring(0, slashOffset)),
+                Name = Uri.UnescapeDataString(uri.AbsoluteUri.Substring(slashOffset + 1))
             };
         }
 
-        public static SplitUri SplitUri(Uri uri)
-        {
-            return SplitUri(uri.AbsoluteUri);
-        }
-
+        /// <summary>
+        /// Obtain the destination uri from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// Destination for this HTTP request (or <see langword="null"/> if no
+        /// destination is specified).
+        /// </returns>
         public static Uri GetDestinationUri(this IHttpRequest request)
         {
             // Obtain the destination
@@ -55,10 +91,21 @@ namespace NWebDav.Server.Helpers
             if (destinationHeader == null)
                 return null;
 
-            // Return the splitted Uri
+            // Return the destination URI
             return new Uri(destinationHeader);
         }
 
+        /// <summary>
+        /// Obtain the depth value from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// Depth of the HTTP request (<c>int.MaxValue</c> if infinity).
+        /// </returns>
+        /// <remarks>
+        /// If the Depth header is not set, then the specification specifies
+        /// that it should be interpreted as infinity.
+        /// </remarks>
         public static int GetDepth(this IHttpRequest request)
         {
             // Obtain the depth header (no header means infinity)
@@ -75,6 +122,19 @@ namespace NWebDav.Server.Helpers
             return depth;
         }
 
+        /// <summary>
+        /// Obtain the overwrite value from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// Flag indicating whether or not to overwrite the destination
+        /// if it already exists.
+        /// </returns>
+        /// <remarks>
+        /// If the Overwrite header is not set, then the specification
+        /// specifies that it should be interpreted as 
+        /// <see langwordk="true"/>.
+        /// </remarks>
         public static bool GetOverwrite(this IHttpRequest request)
         {
             // Get the Overwrite header
@@ -84,6 +144,17 @@ namespace NWebDav.Server.Helpers
             return overwriteHeader.ToUpperInvariant() == "T";
         }
 
+        /// <summary>
+        /// Obtain the list of timeout values from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// List of timeout values in seconds (<c>-1</c> if infinite).
+        /// </returns>
+        /// <remarks>
+        /// If the Timeout header is not set, then <see langword="null"/> is
+        /// returned.
+        /// </remarks>
         public static IList<int> GetTimeouts(this IHttpRequest request)
         {
             // Get the value of the timeout header as a string
@@ -109,6 +180,13 @@ namespace NWebDav.Server.Helpers
             return timeoutHeader.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(parseTimeout).Where(t => t != 0).ToArray();
         }
 
+        /// <summary>
+        /// Obtain the lock-token URI from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// Lock token URI (<see langword="null"/> if not set).
+        /// </returns>
         public static Uri GetLockToken(this IHttpRequest request)
         {
             // Get the value of the lock-token header as a string
@@ -124,6 +202,13 @@ namespace NWebDav.Server.Helpers
             return new Uri(lockTokenHeader.Substring(1, lockTokenHeader.Length - 2), UriKind.Absolute);
         }
 
+        /// <summary>
+        /// Obtain the if-lock-token URI from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// If lock token URI (<see langword="null"/> if not set).
+        /// </returns>
         public static Uri GetIfLockToken(this IHttpRequest request)
         {
             // Get the value of the lock-token header as a string
@@ -139,6 +224,13 @@ namespace NWebDav.Server.Helpers
             return new Uri(lockTokenHeader.Substring(2, lockTokenHeader.Length - 4), UriKind.Absolute);
         }
 
+        /// <summary>
+        /// Obtain the range value from the request.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// Range value (start/end) with an option if condition.
+        /// </returns>
         public static Range GetRange(this IHttpRequest request)
         {
             // Get the value of the range header as a string
@@ -179,13 +271,21 @@ namespace NWebDav.Server.Helpers
             return range;
         }
 
+        /// <summary>
+        /// Load an XML document from the HTTP request body.
+        /// </summary>
+        /// <param name="request">HTTP request.</param>
+        /// <returns>
+        /// XML document that represents the body content (or 
+        /// <see langword="null"/> if no body content is specified).
+        /// </returns>
         public static XDocument LoadXmlDocument(this IHttpRequest request)
         {
             // If there is no input stream, then there is no XML document
             if (request.Stream == null || request.Stream == Stream.Null)
                 return null;
 
-            // If there is no data,
+            // Return null if no content has been specified
             var contentLengthString = request.GetHeaderValue("Content-Length");
             if (contentLengthString != null)
             {
@@ -193,6 +293,10 @@ namespace NWebDav.Server.Helpers
                 if (!int.TryParse(contentLengthString, out contentLength) || contentLength == 0)
                     return null;
             }
+
+            // Return null if no stream is available
+            if (request.Stream == null || request.Stream == Stream.Null)
+                return null;
 
             // Obtain an XML document from the stream
             var xDocument = XDocument.Load(request.Stream);

NWebDav.Sample.Kestrel/Namespace.cs
@@ -1,0 +1,10 @@
+﻿namespace NWebDav.Server
+{
+    /// <summary>
+    /// Top-level namespace for the NWebDav.Server assembly.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}

NWebDav.Server/DavStatusCode.cs
@@ -20,7 +20,8 @@ namespace NWebDav.Server
     /// status. It will be sent with the response if the underlying HTTP server
     /// allows custom HTTP return codes. Clients should parse the numeric
     /// status code, because it's not guaranteed that the textual version is
-    /// returned.
+    /// returned. If the human readable version is required, then it can be
+    /// obtained using the <see cref="DavStatusCodeHelper"/> helper class.
     /// </para>
     /// <para>
     /// The HTTP status codes are documented on

NWebDav.Server/Handlers/CopyHandler.cs
@@ -8,8 +8,30 @@ using NWebDav.Server.Stores;
 
 namespace NWebDav.Server.Handlers
 {
+    /// <summary>
+    /// Implementation of the COPY method.
+    /// </summary>
+    /// <remarks>
+    /// The specification of the WebDAV COPY method can be found in the
+    /// <see href="http://www.webdav.org/specs/rfc2518.html#METHOD_COPY">
+    /// WebDAV specification
+    /// </see>.
+    /// </remarks>
     public class CopyHandler : IRequestHandler
     {
+        /// <summary>
+        /// Handle a COPY request.
+        /// </summary>
+        /// <param name="httpContext">
+        /// The HTTP context of the request.
+        /// </param>
+        /// <param name="store">
+        /// Store that is used to access the collections and items.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous COPY operation. The task
+        /// will always return <see langword="true"/> upon completion.
+        /// </returns>
         public async Task<bool> HandleRequestAsync(IHttpContext httpContext, IStore store)
         {
             // Obtain request and response
@@ -21,7 +43,7 @@ namespace NWebDav.Server.Handlers
             if (destinationUri == null)
             {
                 // Bad request
-                response.SendResponse(DavStatusCode.BadRequest, "Destination header is missing.");
+                response.SetStatus(DavStatusCode.BadRequest, "Destination header is missing.");
                 return true;
             }
 
@@ -29,7 +51,7 @@ namespace NWebDav.Server.Handlers
             if (request.Url.AbsoluteUri.Equals(destinationUri.AbsoluteUri, StringComparison.CurrentCultureIgnoreCase))
             {
                 // Forbidden
-                response.SendResponse(DavStatusCode.Forbidden, "Source and destination cannot be the same.");
+                response.SetStatus(DavStatusCode.Forbidden, "Source and destination cannot be the same.");
                 return true;
             }
 
@@ -44,7 +66,7 @@ namespace NWebDav.Server.Handlers
             if (destinationCollection == null)
             {
                 // Source not found
-                response.SendResponse(DavStatusCode.Conflict, "Destination cannot be found or is not a collection.");
+                response.SetStatus(DavStatusCode.Conflict, "Destination cannot be found or is not a collection.");
                 return true;
             }
 
@@ -53,7 +75,7 @@ namespace NWebDav.Server.Handlers
             if (sourceItem == null)
             {
                 // Source not found
-                response.SendResponse(DavStatusCode.NotFound, "Source cannot be found.");
+                response.SetStatus(DavStatusCode.NotFound, "Source cannot be found.");
                 return true;
             }
 
@@ -78,7 +100,7 @@ namespace NWebDav.Server.Handlers
             else
             {
                 // Set the response
-                response.SendResponse(DavStatusCode.Ok);
+                response.SetStatus(DavStatusCode.Ok);
             }
 
             return true;

NWebDav.Server/Handlers/DeleteHandler.cs
@@ -8,8 +8,30 @@ using NWebDav.Server.Stores;
 
 namespace NWebDav.Server.Handlers
 {
+    /// <summary>
+    /// Implementation of the DELETE method.
+    /// </summary>
+    /// <remarks>
+    /// The specification of the WebDAV DELETE method can be found in the
+    /// <see href="http://www.webdav.org/specs/rfc2518.html#METHOD_DELETE">
+    /// WebDAV specification
+    /// </see>.
+    /// </remarks>
     public class DeleteHandler : IRequestHandler
     {
+        /// <summary>
+        /// Handle a DELETE request.
+        /// </summary>
+        /// <param name="httpContext">
+        /// The HTTP context of the request.
+        /// </param>
+        /// <param name="store">
+        /// Store that is used to access the collections and items.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous DELETE operation. The task
+        /// will always return <see langword="true"/> upon completion.
+        /// </returns>
         public async Task<bool> HandleRequestAsync(IHttpContext httpContext, IStore store)
         {
             // Obtain request and response
@@ -27,7 +49,7 @@ namespace NWebDav.Server.Handlers
             if (parentCollection == null)
             {
                 // Source not found
-                response.SendResponse(DavStatusCode.NotFound);
+                response.SetStatus(DavStatusCode.NotFound);
                 return true;
             }
 
@@ -36,7 +58,7 @@ namespace NWebDav.Server.Handlers
             if (deleteItem == null)
             {
                 // Source not found
-                response.SendResponse(DavStatusCode.NotFound);
+                response.SetStatus(DavStatusCode.NotFound);
                 return true;
             }
 
@@ -47,7 +69,7 @@ namespace NWebDav.Server.Handlers
                 var ifToken = request.GetIfLockToken();
                 if (!deleteItem.LockingManager.HasLock(deleteItem, ifToken))
                 {
-                    response.SendResponse(DavStatusCode.Locked);
+                    response.SetStatus(DavStatusCode.Locked);
                     return true;
                 }
 
@@ -68,7 +90,7 @@ namespace NWebDav.Server.Handlers
             else
             {
                 // Return the proper status
-                response.SendResponse(status);
+                response.SetStatus(status);
             }
 
 

NWebDav.Server/Handlers/GetAndHeadHandler.cs
@@ -9,8 +9,31 @@ using NWebDav.Server.Stores;
 
 namespace NWebDav.Server.Handlers
 {
+    /// <summary>
+    /// Implementation of the GET and HEAD method.
+    /// </summary>
+    /// <remarks>
+    /// The specification of the WebDAV GET and HEAD methods for collections
+    /// can be found in the
+    /// <see href="http://www.webdav.org/specs/rfc2518.html#rfc.section.8.4">
+    /// WebDAV specification
+    /// </see>.
+    /// </remarks>
     public class GetAndHeadHandler : IRequestHandler
     {
+        /// <summary>
+        /// Handle a GET or HEAD request.
+        /// </summary>
+        /// <param name="httpContext">
+        /// The HTTP context of the request.
+        /// </param>
+        /// <param name="store">
+        /// Store that is used to access the collections and items.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous GET or HEAD operation. The
+        /// task will always return <see langword="true"/> upon completion.
+        /// </returns>
         public async Task<bool> HandleRequestAsync(IHttpContext httpContext, IStore store)
         {
             // Obtain request and response
@@ -28,7 +51,7 @@ namespace NWebDav.Server.Handlers
             if (entry == null)
             {
                 // Set status to not found
-                response.SendResponse(DavStatusCode.NotFound);
+                response.SetStatus(DavStatusCode.NotFound);
                 return true;
             }
 
@@ -63,7 +86,7 @@ namespace NWebDav.Server.Handlers
                 if (stream != null && stream != Stream.Null)
                 {
                     // Set the response
-                    response.SendResponse(DavStatusCode.Ok);
+                    response.SetStatus(DavStatusCode.Ok);
 
                     // Set the expected content length
                     try
@@ -98,7 +121,7 @@ namespace NWebDav.Server.Handlers
 
                                 // Set status to partial result if not all data can be sent
                                 if (length < stream.Length)
-                                    response.SendResponse(DavStatusCode.PartialContent);
+                                    response.SetStatus(DavStatusCode.PartialContent);
                             }
 
                             // Set the header, so the client knows how much data is required
@@ -117,7 +140,7 @@ namespace NWebDav.Server.Handlers
                 else
                 {
                     // Set the response
-                    response.SendResponse(DavStatusCode.NoContent);
+                    response.SetStatus(DavStatusCode.NoContent);
                 }
             }
             return true;

NWebDav.Server/Handlers/LockHandler.cs
@@ -11,8 +11,30 @@ using NWebDav.Server.Stores;
 
 namespace NWebDav.Server.Handlers
 {
+    /// <summary>
+    /// Implementation of the LOCK method.
+    /// </summary>
+    /// <remarks>
+    /// The specification of the WebDAV LOCK method can be found in the
+    /// <see href="http://www.webdav.org/specs/rfc2518.html#METHOD_LOCK">
+    /// WebDAV specification
+    /// </see>.
+    /// </remarks>
     public class LockHandler : IRequestHandler
     {
+        /// <summary>
+        /// Handle a LOCK request.
+        /// </summary>
+        /// <param name="httpContext">
+        /// The HTTP context of the request.
+        /// </param>
+        /// <param name="store">
+        /// Store that is used to access the collections and items.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous LOCK operation. The task
+        /// will always return <see langword="true"/> upon completion.
+        /// </returns>
         public async Task<bool> HandleRequestAsync(IHttpContext httpContext, IStore store)
         {
             // Obtain request and response
@@ -28,7 +50,7 @@ namespace NWebDav.Server.Handlers
             if (item == null)
             {
                 // Set status to not found
-                response.SendResponse(DavStatusCode.PreconditionFailed);
+                response.SetStatus(DavStatusCode.PreconditionFailed);
                 return true;
             }
 
@@ -37,7 +59,7 @@ namespace NWebDav.Server.Handlers
             if (lockingManager == null)
             {
                 // Set status to not found
-                response.SendResponse(DavStatusCode.PreconditionFailed);
+                response.SetStatus(DavStatusCode.PreconditionFailed);
                 return true;
             }
 
@@ -94,7 +116,7 @@ namespace NWebDav.Server.Handlers
                 }
                 catch (Exception)
                 {
-                    response.SendResponse(DavStatusCode.BadRequest);
+                    response.SetStatus(DavStatusCode.BadRequest);
                     return true;
                 }
 
@@ -106,7 +128,7 @@ namespace NWebDav.Server.Handlers
             if (lockResult.Result != DavStatusCode.Ok)
             {
                 // Set status to not found
-                response.SendResponse(lockResult.Result);
+                response.SetStatus(lockResult.Result);
                 return true;
             }
 

NWebDav.Server/Handlers/MkcolHandler.cs
@@ -6,8 +6,30 @@ using NWebDav.Server.Stores;
 
 namespace NWebDav.Server.Handlers
 {
+    /// <summary>
+    /// Implementation of the MKCOL method.
+    /// </summary>
+    /// <remarks>
+    /// The specification of the WebDAV MKCOL method can be found in the
+    /// <see href="http://www.webdav.org/specs/rfc2518.html#METHOD_MKCOL">
+    /// WebDAV specification
+    /// </see>.
+    /// </remarks>
     public class MkcolHandler : IRequestHandler
     {
+        /// <summary>
+        /// Handle a MKCOL request.
+        /// </summary>
+        /// <param name="httpContext">
+        /// The HTTP context of the request.
+        /// </param>
+        /// <param name="store">
+        /// Store that is used to access the collections and items.
+        /// </param>
+        /// <returns>
+        /// A task that represents the asynchronous MKCOL operation. The task
+        /// will always return <see langword="true"/> upon completion.
+        /// </returns>
         public async Task<bool> HandleRequestAsync(IHttpContext httpContext, IStore store)
         {
             // Obtain request and response
@@ -22,7 +44,7 @@ namespace NWebDav.Server.Handlers
             if (collection == null)
             {
                 // Source not found
-                response.SendResponse(DavStatusCode.Conflict);
+                response.SetStatus(DavStatusCode.Conflict);
                 return true;
             }
 
@@ -30,7 +52,7 @@ namespace NWebDav.Server.Handlers
             var result = await collection.CreateCollectionAsync(splitUri.Name, false, httpContext).ConfigureAwait(false);
 
             // Finished
-            response.SendResponse(result.Result);
+            response.SetStatus(result.Result);
             return true;
         }
     }

commit 458674d015ec6a2ad52d48183c8a751434e3214c

Fix compilation error in debug mode (introduced in commit 9e314680ab9b0b957897945828bd98b7752ad7f3).

NWebDav.Server/Helpers/RequestHelper.cs
@@ -52,6 +52,9 @@ namespace NWebDav.Server.Helpers
     /// </summary>
     public static class RequestHelper
     {
+#if DEBUG
+        private static readonly NWebDav.Server.Logging.ILogger s_log = NWebDav.Server.Logging.LoggerFactory.CreateLogger(typeof(ResponseHelper));
+#endif
         private static readonly Regex s_rangeRegex = new Regex("bytes\\=(?<start>[0-9]*)-(?<end>[0-9]*)");
 
         /// <summary>