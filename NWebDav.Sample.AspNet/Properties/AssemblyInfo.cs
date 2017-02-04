4ba376481f32c9d9e0f83cbfe38f5c3e5fd3c236
Ramon de Klein <mail@ramondeklein.nl>
2017-02-04 15:31:50 +0100

    Bumped version.

--- NWebDav.Sample.AspNet/Properties/AssemblyInfo.cs
+++ NWebDav.Sample.AspNet/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 // associated with an assembly.
 [assembly: AssemblyTitle("NWebDav.Sample.AspNet")]
 [assembly: AssemblyDescription("WebDAV server sample application (using ASP.NET)")]
-[assembly: AssemblyInformationalVersion("0.1.21.0 (beta)")]
+[assembly: AssemblyInformationalVersion("0.1.22.0 (beta)")]
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
 #else
@@ -25,5 +25,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.21.0")]
-[assembly: AssemblyFileVersion("0.1.21.0")]
+[assembly: AssemblyVersion("0.1.22.0")]
+[assembly: AssemblyFileVersion("0.1.22.0")]
