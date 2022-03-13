758843a5a1cb136750dda910638a0010f5f1053a
MichaelPeter <michaelpeteronline@googlemail.com>
2022-03-13 22:19:58 +0100

    add missing await

--- NWebDav.Server/Handlers/UnlockHandler.cs
+++ NWebDav.Server/Handlers/UnlockHandler.cs
@@ -58,7 +58,7 @@ namespace NWebDav.Server.Handlers
             }
 
             // Perform the lock
-            var result = lockingManager.UnlockAsync(item, lockToken);
+            var result = await lockingManager.UnlockAsync(item, lockToken);
 
             // Send response
             response.SetStatus(result);
