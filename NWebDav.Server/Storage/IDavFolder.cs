a50cef001a2ad7ce13193f55d5623d73582f9e98
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2023-07-06 20:11:51 +0200

    Updated IDavFolder

--- NWebDav.Server/Storage/IDavFolder.cs
+++ NWebDav.Server/Storage/IDavFolder.cs
@@ -1,7 +1,7 @@
 ﻿using NWebDav.Server.Enums;
+using SecureFolderFS.Sdk.Storage.DirectStorage;
 using SecureFolderFS.Sdk.Storage.ExtendableStorage;
 using SecureFolderFS.Sdk.Storage.LocatableStorage;
-using SecureFolderFS.Sdk.Storage.ModifiableStorage;
 using SecureFolderFS.Sdk.Storage.NestedStorage;
 
 namespace NWebDav.Server.Storage
@@ -9,7 +9,7 @@ namespace NWebDav.Server.Storage
     /// <summary>
     /// Represents a WebDAV folder.
     /// </summary>
-    public interface IDavFolder : IDavStorable, ILocatableFolder, IModifiableFolder, IFolderExtended, INestedFolder
+    public interface IDavFolder : IDavStorable, ILocatableFolder, IFolderExtended, INestedFolder, IDirectCopy, IDirectMove
     {
         /// <summary>
         /// Gets the depth mode for enumerating directory contents.
