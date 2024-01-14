abdd1ab8d05aadc9b49b6646bece9c2c70d749e2
Ramon de Klein <mail@ramondeklein.nl>
2024-01-14 22:18:23 +0100

    Use test-directory.

--- NWebDav.Sample.Kestrel/Program.cs
+++ NWebDav.Sample.Kestrel/Program.cs
@@ -1,10 +1,17 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using NWebDav.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddNWebDav()
-    .AddDiskStore();
+    .AddDiskStore(opts =>
+    {
+        var nwebDavDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "NWebDAV");
+        Directory.CreateDirectory(nwebDavDir);
+        opts.BaseDirectory = nwebDavDir;
+    });
 var app = builder.Build();
 app.UseNWebDav();
 app.Run();
